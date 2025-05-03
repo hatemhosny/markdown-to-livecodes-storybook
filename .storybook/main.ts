6dd906c30a01c81daa26efdca5701a1754d4da13
Hatem Hosny <hatemhosny@gmail.com>
2025-05-04 02:48:30 +0300

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,34 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import remarkLivecodes from "remark-livecodes";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [
+              [
+                remarkLivecodes,
+                {
+                  appUrl: "https://dev.livecodes.io",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
